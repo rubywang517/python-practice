8b42b181cd8a1731c95f681387fbc719da0d554f
Ruby <ruby@pfei-2.local>
2020-08-02 20:38:34 -0500

    add class and static method

---
+++ 3-Class-Static-Methods/oop.py
@@ -0,0 +1,62 @@
+class Employee:
+
+    raise_amt = 1.04 
+
+    def __init__(self, first, last, pay):
+        self.first = first
+        self.last = last
+        self.pay = pay
+
+    def fullname(self):
+        return f'{self.first} {self.last}'
+
+    def apply_raise(self):
+        return int(self.pay * self.raise_amt)
+        
+    @classmethod
+    def set_raise_amt(cls, amount):
+        cls.raise_amt = amount
+
+    @classmethod
+    def from_string(cls, emp_str):
+        first,last, pay = emp_str.split('-')
+        return cls(first,last, pay)
+
+    @staticmethod
+    def is_workday(day):
+        if day.weekday() == 5 or day.weekday() == 6:
+            return False
+        return True
+
+
+# practice class method
+emp_1 = Employee('Ruby', 'Wang', 50000)
+emp_2 = Employee('Test', 'Employee', 60000)
+
+Employee.set_raise_amt(1.05)
+
+print(Employee.raise_amt)
+print(emp_1.raise_amt)
+print(emp_2.raise_amt)
+
+
+emp_str_1 = 'John-Doe-70000'
+emp_str_2 = 'Steve-Smith-30000'
+emp_str_3 = 'Jane-Doe-90000'
+
+first, last, pay = emp_str_1.split('-')
+
+# two ways to create instance, with the help of class method, we don't need 
+# to parse the string before inputting them
+#new_emp_1 = Employee(first, last, pay)
+new_emp_1 = Employee.from_string(emp_str_1)
+print(new_emp_1.pay)
+
+# practice staticmethod
+import datetime
+
+my_date = datetime.date(2016, 7, 11)
+print(Employee.is_workday(my_date))
+
+
+        
